bbbb41ca740293c8e700442f15d9b898700fbee8
Konsta <konsta09@gmail.com>
2012-12-22 21:24:48 +0200

    build omxcore for msm7x27
    Use msm7x27a registry tables.
    
    Change-Id: I3d82a7a690a5730ab98721ad881359b3a5709051

--- Android.mk
+++ Android.mk
@@ -1,5 +1,10 @@
+QCOM_MEDIA_ROOT := $(call my-dir)
+ifneq ($(filter msm8960 msm7x27,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+endif
+
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-
-include $(call all-subdir-makefiles)
-
+include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
